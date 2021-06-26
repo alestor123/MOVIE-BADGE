10d4f76559931ef1f5c30de8c462cb13be089080
alestor123 <alestoraldous@gmail.com>
2021-06-26 22:05:54 +0530

    added ejs

--- api/currently.js
+++ api/currently.js
@@ -1,3 +1,4 @@
+var {readFileSync} = require('fs')
 module.exports = async (req,res) => {
-    
+res.send('lol')
 }
--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 const movies = require('./App');
 (async () => {
-  const data = await movies('macbudy', process.env.TRAK, process.env.TMDB, 300)
+  const data = await movies('alestor123', process.env.TRAK, process.env.TMDB, 300)
   console.log(data)
 })()
 // console.log(process.env.TMDB)
